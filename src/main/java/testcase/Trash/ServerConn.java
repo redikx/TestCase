ca7ea2447172d4024431f529169d2777cf334cfb
redik <redikx@gmail.com>
2018-06-13 23:17:28 +0200

    no message

---
+++ src/main/java/testcase/Trash/ServerConn.java
@@ -0,0 +1,27 @@
+package testcase.Trash;
+
+public class ServerConn {
+
+    private String ipHost;
+    private int portNumber;
+    
+    //* Constructor
+    public ServerConn(String ipHost, int portNumber) {
+	this.ipHost = ipHost;
+	this.portNumber = portNumber;
+    }
+    
+    public String getIpHost() {
+        return ipHost;
+    }
+    public void setIpHost(String ipHost) {
+        this.ipHost = ipHost;
+    }
+    public int getPortNumber() {
+        return portNumber;
+    }
+    public void setPortNumber(int portNumber) {
+        this.portNumber = portNumber;
+    }
+    
+}
