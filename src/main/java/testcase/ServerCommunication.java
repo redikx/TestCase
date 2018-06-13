481650e02af09cd7215884493bc15fe3ac903a22
redik <redikx@gmail.com>
2018-06-13 23:23:05 +0200

    no message

--- src/main/java/testcase/ServerCommunication.java
+++ src/main/java/testcase/ServerCommunication.java
@@ -14,14 +14,7 @@ import org.slf4j.LoggerFactory;
 public class ServerCommunication  implements Closeable {
 
     private Server server;
-<<<<<<< HEAD
     private Socket sock = null;
-=======
-<<<<<<< HEAD
-    private Socket sock = null;
-=======
->>>>>>> 2e8c80dc26338d67864a021e4d640948371c58c6
->>>>>>> eb9d5e3e3fbc87b4774469ca2a0ae9529da21559
     Logger logger = LoggerFactory.getLogger(ServerCommunication.class);
 
     //* Constructor
@@ -30,10 +23,6 @@ public class ServerCommunication  implements Closeable {
 	this.server = server;
     }
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> eb9d5e3e3fbc87b4774469ca2a0ae9529da21559
     //* Connect method creating socket
     public void connect() throws IOException {
 	this.sock = new Socket(server.getHostName(), server.getPort());
@@ -48,8 +37,6 @@ public class ServerCommunication  implements Closeable {
 	    //* Sending Message
 	    try {
 	    BufferedOutputStream bos = new BufferedOutputStream(sock.getOutputStream());
-<<<<<<< HEAD
-=======
 	    OutputStreamWriter osw = new OutputStreamWriter(bos, "US-ASCII");
 	    String messageToSend = message + '\r';
 	    osw.write(messageToSend);
@@ -79,19 +66,16 @@ public class ServerCommunication  implements Closeable {
 	if (!this.sock.isClosed()) {
 	    this.sock.close();
 	    logger.error("Closing socket");
-=======
     public void SendMessage(String message) throws UnknownHostException, IOException  {
 	Socket socket = new Socket(server.getHostName(),server.getPort());
 	StringBuffer instr = new StringBuffer();
 	logger.info("SocketClient initialized for " + message);
 	try {
 	    BufferedOutputStream bos = new BufferedOutputStream(socket.getOutputStream());
->>>>>>> eb9d5e3e3fbc87b4774469ca2a0ae9529da21559
 	    OutputStreamWriter osw = new OutputStreamWriter(bos, "US-ASCII");
 	    String messageToSend = message + '\r';
 	    osw.write(messageToSend);
 	    osw.flush();
-<<<<<<< HEAD
 	    //InputStream  isr = sock.getInputStream();
 	    	//* Reading Answer
 	    //int carrier;
@@ -117,7 +101,6 @@ public class ServerCommunication  implements Closeable {
 	if (!this.sock.isClosed()) {
 	    this.sock.close();
 	    logger.error("Closing socket");
-=======
 	    BufferedInputStream bis = new BufferedInputStream(socket.getInputStream());
 	    InputStreamReader isr = new InputStreamReader(bis, "US-ASCII");
 	    int carrier;
@@ -128,8 +111,6 @@ public class ServerCommunication  implements Closeable {
 	catch (IOException f) {logger.error(f.getMessage());}
 	catch (Exception e) {logger.error(e.getMessage());}
 	finally { }
->>>>>>> 2e8c80dc26338d67864a021e4d640948371c58c6
->>>>>>> eb9d5e3e3fbc87b4774469ca2a0ae9529da21559
 	}
     }
 }
