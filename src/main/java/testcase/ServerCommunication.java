40db16ce383a9ed983fc813b67e394acbdc68275
redik <redikx@gmail.com>
2018-06-13 23:02:57 +0200

    no message

--- src/main/java/testcase/ServerCommunication.java
+++ src/main/java/testcase/ServerCommunication.java
@@ -1,10 +1,26 @@
 package testcase;
 
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
 import java.io.BufferedOutputStream;
 import java.io.Closeable;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+=======
+import java.io.BufferedInputStream;
+>>>>>>> eb9d5e3e3fbc87b4774469ca2a0ae9529da21559
+import java.io.BufferedOutputStream;
+import java.io.Closeable;
+import java.io.DataInputStream;
+import java.io.IOException;
+<<<<<<< HEAD
+import java.io.InputStream;
+=======
+import java.io.InputStreamReader;
+>>>>>>> 2e8c80dc26338d67864a021e4d640948371c58c6
+>>>>>>> eb9d5e3e3fbc87b4774469ca2a0ae9529da21559
 import java.io.OutputStreamWriter;
 import java.net.Socket;
 
@@ -14,7 +30,14 @@ import org.slf4j.LoggerFactory;
 public class ServerCommunication  implements Closeable {
 
     private Server server;
+<<<<<<< HEAD
     private Socket sock = null;
+=======
+<<<<<<< HEAD
+    private Socket sock = null;
+=======
+>>>>>>> 2e8c80dc26338d67864a021e4d640948371c58c6
+>>>>>>> eb9d5e3e3fbc87b4774469ca2a0ae9529da21559
     Logger logger = LoggerFactory.getLogger(ServerCommunication.class);
 
     //* Constructor
@@ -23,6 +46,10 @@ public class ServerCommunication  implements Closeable {
 	this.server = server;
     }
 
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+>>>>>>> eb9d5e3e3fbc87b4774469ca2a0ae9529da21559
     //* Connect method creating socket
     public void connect() throws IOException {
 	this.sock = new Socket(server.getHostName(), server.getPort());
@@ -37,10 +64,50 @@ public class ServerCommunication  implements Closeable {
 	    //* Sending Message
 	    try {
 	    BufferedOutputStream bos = new BufferedOutputStream(sock.getOutputStream());
+<<<<<<< HEAD
+=======
 	    OutputStreamWriter osw = new OutputStreamWriter(bos, "US-ASCII");
 	    String messageToSend = message + '\r';
 	    osw.write(messageToSend);
 	    osw.flush();
+	    InputStream  isr = sock.getInputStream();
+	    	//* Reading Answer
+	    int carrier;
+	    logger.debug("Reading answer" );    
+//* Commented to check connection to Linux
+	    	 //InputStream inFromServer = sock.getInputStream();
+	         //DataInputStream in = new DataInputStream(inFromServer);
+	         //logger.info("Server says " + in.readUTF());
+	         
+	    while ( (carrier = isr.read()) != 'A') {
+	    	    logger.debug("Received=" + carrier);
+	    	    instr.append((char)carrier);	    	    
+	    	}
+	    logger.debug("End of sending : " + messageToSend);
+	    }
+	    catch (IOException e) {logger.error("Error while sending : " + e.getMessage());}
+	} 
+	else {logger.error("Socket is not connected");}
+    }
+    
+    //* Close method closing socket
+    public void close() throws IOException {
+	if (!this.sock.isClosed()) {
+	    this.sock.close();
+	    logger.error("Closing socket");
+=======
+    public void SendMessage(String message) throws UnknownHostException, IOException  {
+	Socket socket = new Socket(server.getHostName(),server.getPort());
+	StringBuffer instr = new StringBuffer();
+	logger.info("SocketClient initialized for " + message);
+	try {
+	    BufferedOutputStream bos = new BufferedOutputStream(socket.getOutputStream());
+>>>>>>> eb9d5e3e3fbc87b4774469ca2a0ae9529da21559
+	    OutputStreamWriter osw = new OutputStreamWriter(bos, "US-ASCII");
+	    String messageToSend = message + '\r';
+	    osw.write(messageToSend);
+	    osw.flush();
+<<<<<<< HEAD
 	    //InputStream  isr = sock.getInputStream();
 	    	//* Reading Answer
 	    //int carrier;
@@ -66,6 +133,19 @@ public class ServerCommunication  implements Closeable {
 	if (!this.sock.isClosed()) {
 	    this.sock.close();
 	    logger.error("Closing socket");
+=======
+	    BufferedInputStream bis = new BufferedInputStream(socket.getInputStream());
+	    InputStreamReader isr = new InputStreamReader(bis, "US-ASCII");
+	    int carrier;
+	    	while ( (carrier = isr.read()) != 13) instr.append((char)carrier);
+	    	socket.close();
+	}
+	
+	catch (IOException f) {logger.error(f.getMessage());}
+	catch (Exception e) {logger.error(e.getMessage());}
+	finally { }
+>>>>>>> 2e8c80dc26338d67864a021e4d640948371c58c6
+>>>>>>> eb9d5e3e3fbc87b4774469ca2a0ae9529da21559
 	}
     }
 }
--- src/main/java/testcase/TestCaseRun.java
+++ src/main/java/testcase/TestCaseRun.java
@@ -1,6 +1,14 @@
 package testcase;
 
+<<<<<<< HEAD
 import java.io.IOException;
+=======
+<<<<<<< HEAD
+import java.io.IOException;
+=======
+import java.net.ConnectException;
+>>>>>>> 2e8c80dc26338d67864a021e4d640948371c58c6
+>>>>>>> eb9d5e3e3fbc87b4774469ca2a0ae9529da21559
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -35,6 +43,30 @@ public class TestCaseRun {
 	serverCommunication.close();
     }
 
+<<<<<<< HEAD
+    private static Logger logger = LoggerFactory.getLogger(TestCaseRun.class);
+
+    public static void execute(Server server, TestCase tc) throws IOException, InterruptedException {
+	// *
+	ServerCommunication serverCommunication = new ServerCommunication(server);
+	serverCommunication.connect();
+
+	tc.readFile();
+	for (String cur : tc) {
+	    try {
+		Thread.sleep(100);
+	    } catch (InterruptedException e) {
+		logger.warn(e.getMessage());
+	    }
+
+	    logger.debug("Sending:" + cur);
+	    try {
+		serverCommunication.sendMessage(cur);
+		Thread.sleep(1000);
+	    } catch (IOException c) {
+		logger.error(c.getMessage());
+		throw c;
+=======
     public static void main(String args[]) throws Exception {
 
 	Logger logger = LoggerFactory.getLogger(TestCase.class);
@@ -47,5 +79,29 @@ public class TestCaseRun {
 	execute(server, testCase);
 	execute(server, testCase2);
 
+<<<<<<< HEAD
+=======
+>>>>>>> 2e8c80dc26338d67864a021e4d640948371c58c6
+	    }
+	}
+
+	serverCommunication.close();
+
+    }
+
+    public static void main(String args[]) throws Exception {
+
+	Logger logger = LoggerFactory.getLogger(TestCase.class);
+	logger.info("Start files reading");
+	TestCase testCase = new TestCase("c:\\Java\\Projects\\source_file1.txt");
+	TestCase testCase2 = new TestCase("c:\\Java\\Projects\\source_file2.txt");
+	logger.info("Files loaded, connecting");
+
+        Server server = new Server("10.242.44.22",31015);
+	//Server server = new Server("192.168.198.130", 31015);
+	execute(server, testCase);
+	execute(server, testCase2);
+
+>>>>>>> eb9d5e3e3fbc87b4774469ca2a0ae9529da21559
     }
 }
